5c61d9877265b13345b762cec524b1d3177e5128
= <=>
2021-04-14 20:05:18 +0530

    ...

--- assignment_4/App.java
+++ assignment_4/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String args[]) {
         try {
             BookARide ride = new BookARide();
+            //this will throw exception if argument's value exceeds 4
             ride.bookSeat(2);
         } catch (Exception e) {
             e.printStackTrace();
